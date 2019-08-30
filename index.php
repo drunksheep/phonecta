e92d741d153446ec12610a9c92877c6389e65dfb
André Facchini <andre.facchini@3xceler.com.br>
2019-08-30 10:49:30 -0300

    Primeira versão funcional

--- index.php
+++ index.php
@@ -7,8 +7,8 @@
 	<link rel="stylesheet" href="dist/main.css">
 </head>
 <body>
-	<!-- O CTA --> 
-	<div class="cta-open-wrapper">
+	<!-- O CTA -->
+	<div class="cta-open-wrapper cta-shown">
 		<div class="cta-tooltip">
 			<p class="cta-main-title">Olá!</p>
 			<p class="cta-main-text">
@@ -25,18 +25,82 @@
 			<i class="fa fa-phone"></i>
 		</button>
 	</div>
-	<!-- Fim do CTA --> 
+	<!-- Fim do CTA -->
 
 	<!-- Overlay/Corpo dos formulários  -->
 
 	<div class="cta-overlay">
-		<div class="form-wrapper-all">
-			
+		<button class="close-cta-overlay" aria-label="Fechar CTA de Ligação">&times;</button>
+		<div class="cta-form-wrapper-all">
+			<div class="form-pickers">
+				<button class="form-picker" data-instance="me-ligue-agora"><i class="fa fa-phone"></i>ME LIGUE AGORA</button>
+				<button class="form-picker active" data-instance="me-ligue-depois"><i class="fa fa-clock"></i>ME LIGUE DEPOIS</button>
+				<button class="form-picker" data-instance="deixe-sua-mensagem"><i class="far fa-comments"></i>DEIXE UMA MENSAGEM</button>
+			</div>
+			<form class="cta-form nos-te-ligamos-form full" data-instance="me-ligue-agora">
+				<div role="group" class="field-box flexed row spaced centered nos-te-ligamos-fieldset">
+					<legend class="form-title">
+						<span>NÓS TE LIGAMOS</span> <br>
+						Informe seu telefone que entraremos em contato o mais rápido possível.
+					</legend>
+					<label class="field-wrapper full has-icon">
+						<i class="fa fa-user-circle"></i>
+						<input type="text" class="cta-field has-icon" name="Nome" placeholder="Informe seu Nome*" required>
+					</label>
+					<label class="field-wrapper full has-icon">
+						<i class="fa fa-phone"></i>
+						<input type="text" class="cta-field has-icon" name="Telefone" placeholder="Informe seu Telefone*" required>
+					</label>
+					<button class="submit-cta call-now">ME LIGUE AGORA</button>
+				</div>
+			</form>
+			<form class="cta-form me-ligue-depois-form active" data-instance="me-ligue-depois">
+				<div role="group" class="field-box flexed row spaced centered me-ligue-depois-fieldset">
+					<legend class="form-title">
+						Gostaria de agendar e receber uma chamada em outro horário?
+					</legend>
+					<label class="field-wrapper half">
+						<input type="text" class="cta-field" name="Data" placeholder="Data da Ligação">
+					</label>
+					<label class="field-wrapper half">
+						<input type="time" class="cta-field" name="Horário" placeholder="Horário da Ligação">
+					</label>
+					<label class="field-wrapper full has-icon">
+						<i class="fa fa-user-circle"></i>
+						<input type="text" class="cta-field has-icon" name="Nome" placeholder="Informe seu Nome*" required>
+					</label>
+					<label class="field-wrapper full has-icon">
+						<i class="fa fa-phone"></i>
+						<input type="text" class="cta-field has-icon" name="Telefone" placeholder="Informe seu Telefone*" required>
+					</label>
+					<button class="submit-cta call-later">ME LIGUE DEPOIS</button>
+				</div>
+			</form>
+			<form class="cta-form deixe-sua-mensagem-form" data-instance="deixe-sua-mensagem">
+				<div role="group" class="field-box flexed row spaced centered deixe-sua-mensagem-fieldset">
+					<legend class="form-title">
+						Deixe sua mensagem! Entraremos em
+						contato o mais rápido possível.
+					</legend>
+					<label class="field-wrapper full">
+						<textarea class="cta-field bigger" name="mensagem" placeholder="deixe sua mensagem" required></textarea>
+					</label>
+					<label class="field-wrapper full has-icon">
+						<i class="fa fa-user-circle"></i>
+						<input type="text" class="cta-field has-icon" name="Nome" placeholder="Informe seu Nome*" required>
+					</label>
+					<label class="field-wrapper full has-icon">
+						<i class="fa fa-phone"></i>
+						<input type="text" class="cta-field has-icon" name="Telefone" placeholder="Informe seu Telefone*" required>
+					</label>
+					<button class="submit-cta leave-message">DEIXE SUA MENSAGEM</button>
+				</div>
+			</form>
 		</div>
 	</div>
 
-	<!-- Fim do Overlay/Corpo dos formulários --> 
-	
+	<!-- Fim do Overlay/Corpo dos formulários -->
+
 </body>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script src="dist/app.js"></script>
--- src/js/phone.js
+++ src/js/phone.js
@@ -1,46 +1,150 @@
 class PhoneCTA {
 	constructor(settings) {
 
-		let defaultSettings = {
-			overlay: '.cta-overlay', 
-			formWrapper: '.cta-form-wrapper-all', 
+		const defaultSettings = {
+			overlay: document.querySelector('.cta-overlay'),
+			formWrapper: document.querySelector('.cta-form-wrapper-all'),
+			hideCtaBtn : '.cta-hide-tooltip',
+			showCtaBtn : '.phone-icon',
+			ctaWrapper : document.querySelector('.cta-open-wrapper'),
 			openButton: '.cta-open',
-			closeButton: '.cta-overlay-close',
+			closeButton: '.close-cta-overlay',
+			formSwitchers: '.form-picker',
+			ctaForms: '.cta-form'
 		};
 
+		// decides between default settings or passed args on constructor
 		this.settings = arguments.length < 1 ? defaultSettings : settings;
 
-		this.debugSettings();
+		// this.debugSettings();
+
+		// Open button bind
+		this.addEvent(this.settings.openButton, 'click', this.openModal.bind(this));
+
+		// Close button bind
+		this.addEvent(this.settings.closeButton, 'click', this.closeModal.bind(this));
+
+		// Form Switchers
+		this.addEvent(this.settings.formSwitchers, 'click', this.formSwitcher.bind(this));
+
+		// hide / show tooltip
+		this.addEvent(this.settings.hideCtaBtn, 'click', this.toggleCTA.bind(this));
+		this.addEvent(this.settings.showCtaBtn, 'click', this.toggleCTA.bind(this));
 	}
+
 
 	/**
 	 * @method debugSettings
-	 * Logs settings currently being used 
-	 */
+	 * Logs settings currently being used
+	*/
 
 	debugSettings() {
 		console.log(this.settings);
 	}
 
 	/**
-	 * @method AddEvent  
-	 * @param {String} element 
-	 * @param {String} event 
-	 * @param {String} func 
-	 * 
-	 * Helper methods to bind events;
-	 */
-	
+	 * @method AddEvent
+	 * @param {DomTokenNode | DomTokenList} element // Nodes to bind event
+	 * @param {String} event  // Event to bind
+	 * @param {String | Function} func // function to use on event
+	 *
+	 * Helper method to bind events;
+	*/
+
 	addEvent(element, event, func) {
-		
+
+		if (element !== window) {
+			element = document.querySelectorAll(element);
+
+			element.forEach(el => {
+				return el.addEventListener(event, func);
+			});
+		} else {
+			return element.addEventListener(event, func);
+		}
+
+	}
+
+	/**
+	 * @param {DOMTokenNode} el Element to apply
+	 * @param {Function} cb Callback Function
+	 * @see https://gist.github.com/alirezas/c4f9f43e9fe1abba9a4824dd6fc60a55
+	 * FadeOut polyfill for vanilla JS
+	*/
+
+	fadeOut(el, cb) {
+		el.style.opacity = 1;
+
+		(function fade() {
+			if ((el.style.opacity -= .1) < 0) {
+				el.style.display = "none";
+			} else {
+				requestAnimationFrame(fade);
+			}
+		})();
+
+		if (typeof cb === 'function') {
+			cb();
+		}
+	}
+
+	/**
+	 * @param {DOMTokenNode} el Element to apply
+	 * @param {String} display What display property should be applied to the element after fading in. Defaults to Block
+	 * @param {Function} cb Callback Function
+	 * @see https://gist.github.com/alirezas/c4f9f43e9fe1abba9a4824dd6fc60a55
+	 * FadeIn polyfill for vanilla JS
+	*/
+
+	fadeIn(el, display, cb) {
+		el.style.opacity = 0;
+		el.style.display = display || "block";
+
+		(function fade() {
+			var val = parseFloat(el.style.opacity);
+			if (!((val += .1) > 1)) {
+				el.style.opacity = val;
+				requestAnimationFrame(fade);
+			}
+		})();
+
+		if ( typeof cb === 'function' ) {
+			cb();
+		}
 	}
 
 	openModal() {
-
+		this.fadeIn(this.settings.overlay);
 	}
 
 	closeModal() {
-		
+		this.fadeOut(this.settings.overlay);
+	}
+
+	formSwitcher(event) {
+		// Prevent clicks on <i> icon missing instance data;
+		let target = event.target.nodeName === 'I' ? event.target.parentNode : event.target;
+		let instance = target.dataset.instance;
+		let forms = document.querySelectorAll(this.settings.ctaForms);
+		let targetedForm = document.querySelector(`${this.settings.ctaForms}[data-instance="${instance}"]`);
+		let switchers = document.querySelectorAll(this.settings.formSwitchers);
+
+
+		forms.forEach(switcher => {
+			this.fadeOut(switcher);
+		});
+
+		document.querySelector(`${this.settings.formSwitchers}.active`).classList.remove('active');
+		target.classList.add('active');
+
+		window.setTimeout( () => {
+			this.fadeIn(targetedForm);
+		}, 400);
+
+	}
+
+	toggleCTA() {
+		this.settings.ctaWrapper.classList.toggle('cta-shown');
 	}
 
 }
@@ -48,5 +152,4 @@ class PhoneCTA {
 
 window.onload = () => {
 	let CTA = new PhoneCTA;
-	// PhoneCTA.debugSettings();
 }
