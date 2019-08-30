070acb2dd152d8e18be1625572c5a88c610106c6
André Facchini <andre.facchini@3xceler.com.br>
2019-08-30 10:55:14 -0300

    Edição no readme.md

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
 	<title>Phone CTA</title>
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
 	<link rel="stylesheet" href="dist/main.css">
+	<meta name="viewport" content="width=device-width, user-scalable=no">
 </head>
 <body>
 	<!-- O CTA -->
--- readme.md
+++ readme.md
@@ -1,3 +1,5 @@
-## phone CTA ## 
+## phone CTA : sem jQuery ##
 
-Um modo fácil de inserir CTAs ao seu site.
+Um modo fácil de inserir CTAs de telefone ao seu site.
+
+Retrabalhado desde o começo para ser mais fácil de implementar, sem necessidade de jQuery.
